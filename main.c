e6d74b108c9de5a53ffdc94f3e5d7b1c19fea7c0
1darko <dakojic@student.42.fr>
2025-11-14 13:50:11 +0100

    start

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "traceroute.h"
+
+int main(int ac, char **av)
+{
+    printf("starting\n");
+    return 0;
+}
---
+++ traceroute.h
@@ -0,0 +1,27 @@
+#ifndef TRACEROUTE_H
+# define TRACEROUTE_H
+
+#include <sys/time.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <net/ethernet.h>   /* ETH_ALEN */
+#include <net/if.h>
+#include <net/if_arp.h>     /* ARP hardware types */
+#include <netpacket/packet.h> /* sockaddr_ll (AF_PACKET) */
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <limits.h>
+#include <signal.h>
+
+#endif
+
+
